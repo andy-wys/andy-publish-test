d80aa6ed3d74c95a5889853557d4f3f825f89977
andy <win58@qq.com>
2025-01-23 14:37:51 +0800

    feat: test

---
+++ src/main/java/io/github/andy/PublishTest.java
@@ -0,0 +1,11 @@
+package io.github.andy;
+
+/**
+ * @author: andy
+ * @date: 2025/1/23
+ **/
+public class PublishTest {
+    public static void main(String[] args) {
+        System.out.println("demo");
+    }
+}
